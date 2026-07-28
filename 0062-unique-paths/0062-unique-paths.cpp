a728b835fa14913ca1fdcaf86fe3bbf7c2aa3fa7
niyatipandey <niyatipm25@gmail.com>
2026-07-28 23:45:52 +0530

    Time: 3 ms (9.63%), Space: 9.4 MB (30.8%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,0));
+
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if (i == m - 1 && j == n - 1){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int right =0;
+                int bottom =0;
+                if(j+1 < n){
+                    right = dp[i][j+1];
+                }
+                if(i+1 < m){
+                    bottom = dp[i+1][j];
+                }
+                dp[i][j] = right+bottom;
+            }
+        }
+        return dp[0][0];
+    }
+};
