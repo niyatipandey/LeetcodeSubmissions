4729279a7f8fef087e448fef0589105b85311f5e
niyatipandey <niyatipm25@gmail.com>
2026-08-01 11:12:18 +0530

    Time: 2 ms (16.6%), Space: 13.2 MB (59.44%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    const int INF = 1e9;
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        vector<vector<int>>dp(m,vector<int>(m,INF));
-        for(int i=m-1;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                if(i == m-1){
-                    dp[i][j] = triangle[i][j];
-                    continue;
-                }
-                int rightBelow =dp[i+1][j+1];
-                int justBelow =dp[i+1][j];
-
-                dp[i][j] = triangle[i][j] + min(justBelow,rightBelow);
-            }
+    int f(int i,int j,int row,vector<vector<int>>& triangle,vector<vector<int>>& dp){
+        if(i <0 || i>row || j<0 || j>i){
+            return 1e8;
         }
-        return dp[0][0];
+        if(i == row-1){
+            return dp[i][j] = triangle[i][j];
+        }
+        if(dp[i][j] != 1e8){
+            return dp[i][j];
+        }
+        int justMax = f(i+1,j,row,triangle,dp);
+        int sideMax = f(i+1,j+1,row,triangle,dp);
+
+        int mini = min(justMax,sideMax) + triangle[i][j];
+
+        return dp[i][j] = mini;
     }
 
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int row = triangle.size();
+        vector<vector<int>>dp(row,vector<int>(row,1e8));
+
+        return f(0,0,row,triangle,dp);
+    }
 };
