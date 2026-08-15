2a19bcbfbf5b22c5bc53205db4855129a0bd12fa
niyatipandey <niyatipm25@gmail.com>
2026-08-15 05:43:19 +0530

    Time: 3 ms (78.07%), Space: 11.8 MB (15.86%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,43 +3,45 @@ public:
     string minWindow(string s, string t) {
         vector<int>freq1(256,0);
         vector<int>freq2(256,0);
-
-        for(int i=0;i<t.length();i++){
-            freq2[t[i]]++;
+        if(t.empty()){
+            return "";
         }
 
-        int count =0;
+        int n = s.length();
+        int m = t.length();
+
+        for(int i=0;i<m;i++){
+            freq1[t[i]]++;
+        }
+
         int i=0;
         int j=0;
-        int len = INT_MAX;
-        int start =0;
+        int len =INT_MAX;
+        int start = 0;
+        int count =0;
 
-        while(j < s.length()){
-            freq1[s[j]]++;
-
-            if(freq1[s[j]] <= freq2[s[j]]){
+        while(j < n){
+            if(freq2[s[j]] < freq1[s[j]]){
                 count++;
             }
+            freq2[s[j]]++;
 
-            while(count == t.length()){
-                if(j-i+1 < len){
-                    len = j-i+1;
-                    start =i;
+            while(count == m){
+                if(len > j-i+1){
+                    start = i;
                 }
-
-                freq1[s[i]]--;
-                if(freq1[s[i]] < freq2[s[i]]){
+                len = min(len, j-i+1);
+                freq2[s[i]]--;
+                if(freq2[s[i]] < freq1[s[i]]){
                     count--;
                 }
                 i++;
             }
             j++;
         }
-
-        if(len == INT_MAX){
-            return  "";
+        if( len == INT_MAX){
+            return "";
         }
-
         return s.substr(start,len);
     }
 };
