855eacd0385fe2284edf90440e439992320ed754
niyatipandey <niyatipm25@gmail.com>
2026-08-20 16:05:48 +0530

    Time: 0 ms (100%), Space: 11.8 MB (26.09%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -4,26 +4,13 @@ public:
         stack<char>st;
 
         for(int i=0;i<s.length();i++){
-            if(!st.empty()){
-                if(st.top() == 'A' && s[i] == 'B'){
-                    st.pop();
-                }
-                else if(st.top() == 'C' && s[i] == 'D'){
-                    st.pop();
-                }
-                else{
-                    st.push(s[i]);
-                }
+            if(!st.empty() && ((st.top() == 'A' && s[i] == 'B') || (st.top() == 'C' && s[i] == 'D'))){
+                st.pop();
             }else{
                 st.push(s[i]);
             }
         }
 
-        string result ="";
-        while(!st.empty()){
-            result += st.top();
-            st.pop();
-        }
-        return result.length();
+        return st.size();
     }
 };
