d3decce10a59e2192aaade70d08c734b78063d6a
niyatipandey <niyatipm25@gmail.com>
2026-08-26 14:26:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        vector<int>prev(n);
-
-        for(int i=m-1;i>=0;i--){
-            vector<int>curr(n);
-            for(int j=n-1;j>=0;j--){
-                if (i == m - 1 && j == n - 1){
-                    curr[j]=1;
-                }
-                else{
-                    int right =0;
-                    int bottom =0;
-                    if(j+1 < n){
-                        right = curr[j+1];
-                    }
-                    if(i+1 < m){
-                        bottom = prev[j];
-                    }
-                    curr[j] = right+bottom;
-                }
-            }
-            prev=curr;
+    int totalPath(int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i == m && j == n){
+            return 1;
         }
-        return prev[0];
+        if(i < 0 || i > m || j < 0 || j > n){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        int right = totalPath(i,j+1,m,n,dp);
+        int left = totalPath(i+1,j,m,n,dp);
+
+        return dp[i][j] = right+ left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return totalPath(0,0,m-1,n-1,dp);
     }
 };
