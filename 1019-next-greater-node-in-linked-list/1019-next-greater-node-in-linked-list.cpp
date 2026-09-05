e37f2ace13cfd936661d75bd59bc5f62f17f5af2
niyatipandey <niyatipm25@gmail.com>
2026-09-05 18:15:41 +0530

    Time: 6 ms (57.84%), Space: 47.4 MB (18.17%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -20,19 +20,15 @@ public:
         }
 
         vector<int>ans(nums.size(),0);
-        unordered_map<int,int>mpp;
         stack<int>st;
 
 
         for(int i=0;i<nums.size();i++){
             while(!st.empty() && nums[st.top()] < nums[i]){
-                mpp[st.top()] = nums[i];
+                ans[st.top()] = nums[i];
                 st.pop();
             }
             st.push(i);
-        }
-        for(int i=0;i<nums.size();i++){
-            ans[i] = mpp[i];
         }
         return ans;
     }
